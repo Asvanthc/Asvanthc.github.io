fa95b9e91f5a3c6a39460d9ab49bc723a6f6197e
PRADISHWARAN <81158838+PRADISHWARAN@users.noreply.github.com>
2021-08-05 12:47:35 +0530

    Update script.js

--- MSD/script.js
+++ MSD/script.js
@@ -5,20 +5,3 @@ $(document).ready(function () {
   });
 
         
-    function scrollNav() {
-  $('.navbar-nav a').click(function(){  
-    //Toggle Class
-    $(".active").not('.item').removeClass("active");      
-    $(this).closest('li').addClass("active");
-    var theClass = $(this).attr("class");
-    $('.'+theClass).parent('li').addClass('active');
-    //Animate
-    $('html, body').stop().animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    },400);
-    return false;
-  });
-  $('.scrollTop a').scrollTop();
-}
-    scrollNav();
-});
